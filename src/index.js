8c78fb8226f085f0cdb2e4da84cda001a25392cd
SamheinDM <SamheinDM@yandex.ru>
2020-10-08 14:53:47 +0300

    feat: add base function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined) return [];
+
+  let arr = matrix;
+
+  for (let i = 0; i < arr.length; i++) {
+    if ((i + 1) / 2 === 0) {
+      arr[i].reverse();
+    }
+  }
+  let answer = [];
+
+  function plus (el) {
+    answer.push(el);
+  }
+
+  arr.forEach(plus);
+
+  return answer;
 }
